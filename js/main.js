f0d1776aea9328a947d0aadad7583d8bf92d5990
apple <apple@apples-MacBook-Pro.local>
2018-02-08 13:06:18 +0800

    completed nav menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log('jquery is loaded');
+$(document).ready( function() {
+
+  var hamburger = $(".hamburger");
+
+  hamburger.on("click", function(e) {
+
+    hamburger.toggleClass("is-active");
+
+    // Do something else, like open/close menu
+    $('div').toggle('.open-menu');
+});
+});
